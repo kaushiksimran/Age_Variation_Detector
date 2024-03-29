bc0ff21d4d54f0cd53bb56e88a5155de0cc96b40
Simran Kaushik <76643621+kaushiksimran@users.noreply.github.com>
2024-03-29 21:30:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ with st.form("my_form"):
     ##########################################################
 
     if submitted:
-        model = tf.keras.models.load_model("keras_Model.h5", compile=False)
-        class_names = open("labels.txt", "r").readlines()
+        model = tf.keras.models.load_model('keras_Model.h5', compile=False)
+        class_names = open('labels.txt', 'r').readlines()
 
         data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
